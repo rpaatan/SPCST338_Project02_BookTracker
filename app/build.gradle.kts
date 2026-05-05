ec200f2ab1467bc61d0911e2a8f0d3ad7df84cb7
rpaatan <rpaatan@csumb.edu>
2026-05-04 21:53:44 -0700

    Recycler Views - added recyclerAdapter, ToReadActivity, ReadActivity - added recycler_template & activity_display_recycler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
